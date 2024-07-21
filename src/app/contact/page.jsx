f3e64543984dc0223d2d982ff45d12f067b0f7fb
AhmadOthman <trollingosman101@outlook.com>
2024-07-21 23:51:17 +0300

    - Bugs fixed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,10 +6,6 @@ import { Success } from "@/lib/SweetAlert";
 import Image from "next/image";
 import { useState } from "react";
 
-export const metadata = {
-	title: "Contact Page",
-};
-
 const Contact = () => {
 	const [errors, setErrors] = useState({});
 
