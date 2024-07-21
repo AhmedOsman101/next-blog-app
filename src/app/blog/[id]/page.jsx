509dc6b48d97dc9b68f201ce172a9788b3cb68c8
AhmadOthman <trollingosman101@outlook.com>
2024-07-21 23:54:05 +0300

    - Destructuring the id param at single post page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
-const SinglePost = () => {
+const SinglePost = ({ params }) => {
+	const { id } = params;
+
 	return (
 		<section>
 			<div className="container px-6 py-8 mx-auto">
