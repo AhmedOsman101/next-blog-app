5e7440cce9c4aff6b5fd741f3ba219cfa2e44040
AhmadOthman <trollingosman101@outlook.com>
2024-07-24 01:22:32 +0300

    - Changed my prisma prop to include my model directly instead of taking the whole prisma client - Optimized imports - Created `findWith()` method to fetch a user by a certain criteria - Created `create()` method to create a new user

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import DB from "../PrismaConnection";
 import { IPost } from "./Post";
 import { DefaultArgs } from "@prisma/client/runtime/library";
@@ -15,7 +15,7 @@ import { DefaultArgs } from "@prisma/client/runtime/library";
  * @property {IPost[]} [posts] - Array of posts associated with the user.
  */
 export interface IUser {
-	id: string;
+	id?: string;
 	name: string;
 	email: string;
 	password: string;
@@ -31,14 +31,10 @@ export interface IUser {
 export class User {
 	/**
 	 * Prisma client instance for database operations.
-	 * @private
+	 * @public
 	 * @static
 	 */
-	protected static prisma: PrismaClient<
-		Prisma.PrismaClientOptions,
-		never,
-		DefaultArgs
-	> = DB.connection;
+	public static prisma: Prisma.UserDelegate<DefaultArgs> = DB.connection.user;
 
 	/**
 	 * Retrieves all users from the database.
@@ -48,17 +44,19 @@ export class User {
 	 * @returns {Promise<IUser[]>} A promise that resolves to an array of users.
 	 * @throws {Error} If there's an error fetching the users.
 	 */
-	public static async all(withPosts: boolean = false): Promise<IUser[]> {
+	public static async all(
+		withPosts: boolean = false
+	): Promise<IUser[] | false> {
 		try {
-			const users = await User.prisma.user.findMany({
+			const users = await User.prisma.findMany({
 				include: {
 					posts: withPosts,
 				},
 			});
 			return users;
 		} catch (error) {
-			console.info("Failed to fetch users");
-			throw new Error("Failed to fetch users");
+			console.error("Failed to fetch users", error);
+			return false;
 		}
 	}
 
@@ -74,9 +72,9 @@ export class User {
 	public static async find(
 		id: string,
 		withPosts: boolean = false
-	): Promise<IUser | null> {
+	): Promise<IUser | null | false> {
 		try {
-			const user = User.prisma.user.findUnique({
+			const user = User.prisma.findUnique({
 				where: { id },
 				include: {
 					posts: withPosts,
@@ -87,8 +85,38 @@ export class User {
 
 			return user;
 		} catch (error) {
-			console.info("Failed to fetch the user");
-			throw new Error(error);
+			console.error("Failed to fetch the user", error);
+			return false;
+		}
+	}
+
+	public static async findWith(
+		field: string,
+		value: any
+	): Promise<IUser | null | false> {
+		try {
+			const user = await User.prisma.findFirst({
+				where: {
+					[field]: value,
+				},
+			});
+			return user ?? null;
+		} catch (error) {
+			console.error("Failed to find user by field/value", error);
+			return false;
+		}
+	}
+
+	public static async create(
+		data: Prisma.UserCreateInput
+	): Promise<IUser | false> {
+		try {
+			const user = await User.prisma.create({ data });
+
+			return user;
+		} catch (error) {
+			console.error("Failed to create user", error);
+			return false;
 		}
 	}
 }
