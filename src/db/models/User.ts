2616c81a9b7ba927638aea9f43d6214707d4cf17
AhmadOthman <trollingosman101@outlook.com>
2024-07-23 18:18:56 +0300

    Update User.ts

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -52,7 +52,7 @@ class User {
 		try {
 			const users = await User.prisma.user.findMany({
 				include: {
-					posts: withPosts,
+					Post: withPosts,
 				},
 			});
 			return users;
@@ -79,7 +79,7 @@ class User {
 			const user = User.prisma.user.findUnique({
 				where: { id },
 				include: {
-					posts: withPosts,
+					Post: withPosts,
 				},
 			});
 
