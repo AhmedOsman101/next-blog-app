6c0d9b48fc231eb27a9f5b186d46c9d6f3b887b5
AhmadOthman <trollingosman101@outlook.com>
2024-07-23 17:58:04 +0300

    - Fixed some attributes to be compatible with jsx syntax - Updated tsconfig to include all ts files in my src directory - Finished testing Github login provider

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { auth, signIn } from "@/lib/Auth";
+import { auth, signIn } from "@/lib/auth";
 
 const Login = async () => {
 	const session = await auth();
@@ -26,10 +26,10 @@ const Login = async () => {
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke="currentColor"
-							stroke-width="2">
+							strokeWidth="2">
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
 							/>
 						</svg>
@@ -51,10 +51,10 @@ const Login = async () => {
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke="currentColor"
-							stroke-width="2">
+							strokeWidth="2">
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
 							/>
 						</svg>
